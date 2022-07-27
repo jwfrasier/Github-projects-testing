24520ed2e5d9105eaec54f7b29bb03b725cd544a
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-07-27 12:18:33 -0400

    created route

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -10,8 +10,8 @@ const Andrea = "y no work";
 const winnie = "best dog in the world";
 const west = "west";
 const blake = "blake";
-const jason = "does this work?"
-const Ethan = "I wish I was at ucchi"
+const jason = "does this work?";
+const Ethan = "I wish I was at ucchi";
 const Stacy = "Howdy Howdy";
 const joe = "joe";
 const Andrea = "y no work";
@@ -23,6 +23,11 @@ const Carlos2 = "asdf";
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get("/olivia", (req, res) => {
+  res.send("Winnie is the best dog ever!");
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
