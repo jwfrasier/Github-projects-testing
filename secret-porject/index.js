86060745bacadb40b09d8126ef4317619c4e0052
Joseph <frasier.joseph@gmail.com>
2022-07-27 10:06:31 -0500

    initial setup

---
+++ secret-porject/index.js
@@ -0,0 +1,12 @@
+// make a node app with express and a route that takes in a name and returns a greeting
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
