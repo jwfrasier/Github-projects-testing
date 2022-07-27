6046d0963bfd0092707c060a977b274549df12bc
Amanda Hargrove <amandahargrove98@gmail.com>
2022-07-27 16:59:42 -0400

    apparently changes needed to be pushed up
    Added route to index.js

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -1,45 +1,26 @@
 // make a node app with express and a route that takes in a name and returns a greeting
 
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 3000;
-const amanda = 'this is interesting';
-const Ethan = 'I wish I was at ucchi';
-const Stacy = 'Howdy Howdy';
-const winnie = 'best dog in the world';
-const jason = 'does this work?';
-const joe = 'joe';
-const Andrea = 'y no work';
-const west = 'west';
-const blake = 'blake';
-const Carlos = 'CsilvaD99';
-const Carlos2 = 'asdf';
-const violet = 'hmmm';
+const amanda = "this is interesting";
+const Ethan = "I wish I was at ucchi";
+const Stacy = "Howdy Howdy";
+const winnie = "best dog in the world";
+const jason = "does this work?";
+const joe = "joe";
+const Andrea = "y no work";
+const west = "west";
+const blake = "blake";
+const Carlos = "CsilvaD99";
+const Carlos2 = "asdf";
+const violet = "hmmm";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/stacy', (req, res) => {
-  res.send("It's Me!!!!")});
-
-app.get("/carlos", (req, res) => {
-  res.send("gaming shoes on");
-});
-
-app.get("/stacy", (req, res) => {
-  res.send("It's Me!!!!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.get("/amanda", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get('/rahmin', (req, res) => {
-  res.send('goodbye World!');
-})
-
-app.get("/blake", (req, res) => {
-  res.send("It's Me!!!!");
 });
 
 app.listen(PORT, () => {
