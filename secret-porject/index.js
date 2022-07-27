0d40bf77da4e28245d4935b168d2b8e926019e08
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-07-27 11:55:05 -0400

    variable

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -3,7 +3,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const Ethan = "I wish I was at ucchi";
+const Stacy = "Howdy Howdy";
+const joe = "joe";
+const Andrea = "y no work";
 const winnie = "best dog in the world";
+const west = "west";
+const blake = "blake";
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
