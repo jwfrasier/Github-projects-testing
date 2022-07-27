ea4cbca369f7ab02a415bb71e154b319da928a3d
1ShoukR <rahminshoukoohi@gmail.com>
2022-07-27 11:57:59 -0400

    updated

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -5,15 +5,16 @@ const PORT = process.env.PORT || 3000;
 const Rahmin = 'Broke your code lol';
 const joe = 'joe';
 const Andrea = 'y no work';
-const Rahmin2 = 'this the second one idk if this gonna work or not but yolo amirite?';
+const Rahmin2 =
+  'this the second one idk if this gonna work or not but yolo amirite?';
 const west = 'west';
 
-const Ethan = "I wish I was at ucchi"
-const Stacy = "Howdy Howdy";
-const blake = "blake";
+const Ethan = 'I wish I was at ucchi';
+const Stacy = 'Howdy Howdy';
+const blake = 'blake';
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
