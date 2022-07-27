83f22b3cf6a9a7d9cb61fab5a9027b66cf735b4c
Violet Medina <vpmedina303@gmail.com>
2022-07-27 11:48:16 -0500

    resolving conflicts again

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -49,7 +49,8 @@ app.get('/amanda', (req, res) => {
 
 app.get('/violet', (req, res) => {
   res.send('Hello Violet!');
-  
+});
+
 app.get('/jason', (req, res) => {
   res.send('Hello from Jason!');
 });
