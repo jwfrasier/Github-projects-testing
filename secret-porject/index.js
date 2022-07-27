95e00e1031f047f2a784e122403a59a74b58e979
1ShoukR <rahminshoukoohi@gmail.com>
2022-07-27 11:22:21 -0400

    broke your code haha lol

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -1,7 +1,6 @@
 // make a node app with express and a route that takes in a name and returns a greeting
 
 const express = require("express");
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
