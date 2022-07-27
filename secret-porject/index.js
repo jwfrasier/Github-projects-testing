5192fe31a9b58f9edc497a1ddd6ddf5282fa74f0
Amanda Hargrove <amandahargrove98@gmail.com>
2022-07-27 11:29:54 -0400

    Added my amanda variable

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const amanda = "this is interesting";
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
