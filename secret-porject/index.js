8ec20f87a346d8e46548fc4c96838df9eb9debd3
eyyytran <ltatran94@gmail.com>
2022-07-27 11:30:48 -0400

    deleted app express

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -1,7 +1,7 @@
 // make a node app with express and a route that takes in a name and returns a greeting
 
 const express = require('express')
-const app = express()
+
 const PORT = process.env.PORT || 3000
 const Andrea = 'y no work'
 
