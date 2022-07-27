1e0e23883061a5bf057f462697d5db55d71592ee
eyyytran <ltatran94@gmail.com>
2022-07-27 12:47:51 -0400

    fixed broken route

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -1,32 +1,31 @@
 // make a node app with express and a route that takes in a name and returns a greeting
 
-const express = require("express");
-const PORT = process.env.PORT || 3000;
-const amanda = "this is interesting";
-const Ethan = "I wish I was at ucchi";
-const Stacy = "Howdy Howdy";
-const winnie = "best dog in the world";
-const jason = "does this work?";
-const joe = "joe";
-const Andrea = "y no work";
-const west = "west";
-const blake = "blake";
-const Carlos = "CsilvaD99";
-const Carlos2 = "asdf";
-const violet = "hmmm";
+const express = require('express')
+const PORT = process.env.PORT || 3000
+const amanda = 'this is interesting'
+const Ethan = 'I wish I was at ucchi'
+const Stacy = 'Howdy Howdy'
+const winnie = 'best dog in the world'
+const jason = 'does this work?'
+const joe = 'joe'
+const Andrea = 'y no work'
+const west = 'west'
+const blake = 'blake'
+const Carlos = 'CsilvaD99'
+const Carlos2 = 'asdf'
+const violet = 'hmmm'
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.destroy('/Ethan', (req, res) => {
-  res.send('Mojoe joejoe');
+    res.send('Mojoe joejoe')
 })
 
-pp.post('/andrea', (req, res) => {
+app.post('/andrea', (req, res) => {
     res.send('Andrea')
 })
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -48,8 +47,8 @@ app.get('/amanda', (req, res) => {
 })
 
 app.get('/jason', (req, res) => {
-  res.send('Hello from Jason!');
-});
+    res.send('Hello from Jason!')
+})
 app.get('/rahmin', (req, res) => {
     res.send('goodbye World!')
 })
