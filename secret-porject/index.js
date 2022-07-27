295ac6e476378f6c90305732588f81fbbacc0a3a
Emgula96 <ethan_gula@yahoo.com>
2022-07-27 11:29:39 -0400

    ethang
    new variable Ethan

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const Ethan = "I wish I was at ucchi"
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
