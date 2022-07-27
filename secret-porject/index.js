9c42f092f0043e910f091d31b579c268daf42f6a
1ShoukR <rahminshoukoohi@gmail.com>
2022-07-27 11:24:36 -0400

    hahahahahahahaa xd

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -1,10 +1,10 @@
 // make a node app with express and a route that takes in a name and returns a greeting
 
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const Rahmin = 'Broke your code lol';
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
