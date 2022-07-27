835f564f271bcf3916b7282a2b6fdfa6d114828b
West <76145386+westmcmillen@users.noreply.github.com>
2022-07-27 05:29:54 -1000

    Made a variable in index.js

--- secret-porject/index.js
+++ secret-porject/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const west = "west";
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
